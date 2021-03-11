c705cc9effbe9772c4f2747e4a440ae85372320a
Fabricio <fabricioig863@gmail.com>
2021-03-11 19:03:28 -0300

    Gobarber primeira parte completa

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import React from 'react'
 import { View, StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks'
+
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar backgroundColor="#111114" />
-    <View style={{ flex: 1, backgroundColor: "#111114" }}>
-      <Routes />
-    </View>
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: "#111114" }}>
+        <Routes />
+      </View>
+    </AppProvider>
   </NavigationContainer>
 )
 
